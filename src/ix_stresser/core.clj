e91bb3ad6c6c64044f3f077a82bf73119e1747b4
Pedro Santos <donbonifacio@gmail.com>
2015-03-31 09:00:27 +0100

    Initial import

---
+++ src/ix_stresser/core.clj
@@ -0,0 +1,65 @@
+(ns ix-stresser.core
+  (require [clj-http.client :as client]))
+
+(def url "http://donbonifacio.bizflow-makeover.com")
+(def ports [3001 3002])
+
+(defn access-url
+  [path]
+  (str url ":" (rand-nth ports) "/" path "?api_key=donbonifacio"))
+
+(defn invoice-sequence-from-response
+  [response]
+  (let [body (:body response)
+        [match raw-id] (re-matches #"(?sm).*<sequence_number>(.+)</sequence_number>.*" body)]
+    raw-id))
+
+(defn invoice-id-from-response
+  [response]
+  (let [body (:body response)
+        clean-body (clojure.string/replace body "\n" "")
+        [match raw-id] (re-matches #".*<invoice>  <id>(\d+)</id>.*" clean-body)]
+    (read-string raw-id)))
+
+(defn create-invoice
+  []
+  (try
+    (client/post (access-url "invoices.xml")
+                 {:content-type :xml
+                  :socket-timeout 0
+                  :conn-timeout 0
+                  :throw-exceptions false
+                  :accept :xml
+                  :body (str "<?xml version='1.0' encoding='UTF-8'?>
+                        <invoice>
+                        <date>30/03/2015</date>
+                        <due_date>30/03/2015</due_date>
+                        <client>
+                          <name>Bruce Norris</name>
+                        </client>
+                        <items type='array'>
+                          <item>
+                          <name>Product 1</name>
+                          <description>Cleaning product</description>
+                          <unit_price>10.0</unit_price>
+                          <quantity>1.0</quantity>
+                          <unit>unit</unit>
+                          <discount>10.0</discount>
+                          </item>
+                        </items>
+                        </invoice>")})
+    (catch Exception e nil)))
+
+(defn finalize-invoice
+  [invoice-id]
+  (try
+   (client/put  (access-url (str "invoice/" invoice-id "/change-state.xml"))
+               {:content-type :xml
+                :socket-timeout 0
+                :conn-timeout 0
+                :accept :xml
+                :throw-exceptions false
+                :body (str "<invoice>
+                            <state>finalized</state>
+                            </invoice>")})
+    (catch Exception e nil)))
---
+++ test/ix_stresser/core_test.clj
@@ -0,0 +1,46 @@
+(ns ix-stresser.core-test
+  (:require [clojure.test :refer :all]
+            [ix-stresser.core :refer :all]))
+
+(def threads 5)
+(def docs-per-thread 200)
+
+(deftest create-finalinvoice-test
+  (let [response (create-invoice)
+        invoice-id (invoice-id-from-response response)]
+    (is (< 0 invoice-id))
+    (let [response (finalize-invoice invoice-id)
+          sequence-number (invoice-sequence-from-response response)]
+      (is sequence-number))))
+
+(defn create-many-documents
+  []
+  (println "Creating " docs-per-thread " docs...")
+  (take docs-per-thread
+    (repeatedly
+      (fn []
+        (let [resp1 (create-invoice)
+            invoice-id (invoice-id-from-response resp1)]
+          invoice-id)))))
+
+(defn finalize-documents
+  [doc-ids]
+  (println "Finalizing " (count doc-ids) " docs...")
+  (-> (map (fn [invoice-id]
+             (let [resp2 (future (finalize-invoice invoice-id))
+                   resp3 (future (finalize-invoice invoice-id))
+                   resp1 (finalize-invoice invoice-id)
+                   seq-number (or (invoice-sequence-from-response @resp2)
+                                  (invoice-sequence-from-response @resp3)
+                                  (invoice-sequence-from-response resp1))]
+               seq-number)) doc-ids)))
+
+(defn create-and-finalize
+  []
+  (-> (create-many-documents)
+      (finalize-documents)))
+
+(deftest stress-invoices
+  (println 
+    (map #(deref %)
+      (doall (repeatedly threads (fn [] (future (create-and-finalize))))))))
